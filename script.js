5f60d3b67a8178fbc7fbc989e34a3c33b36887f2
Frugd <frugd.tela@gmail.com>
2025-12-15 23:35:21 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'mr-black': '#0a0a0a',
+                'mr-red': '#b91c1c',
+                'mr-gray': '#1f1f1f',
+                'term-green': '#00ff00',
+            },
+            fontFamily: {
+                'mono': ['"Roboto Mono"', 'monospace'],
+                'retro': ['"VT323"', 'monospace'],
+            }
+        }
+    }
+}
